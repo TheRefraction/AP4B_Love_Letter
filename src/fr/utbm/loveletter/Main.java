5d967f93104fab77e4beda0b4b2608f68489d91c
Corentin Hautefaye <the.r3fraction@gmail.com>
2025-11-26 18:12:10 +0100

    test

--- src/fr/utbm/loveletter/Main.java
+++ src/fr/utbm/loveletter/Main.java
@@ -2,6 +2,6 @@ package fr.utbm.loveletter;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello and welcome!");
+        System.out.println("Hello world!");
     }
 }
