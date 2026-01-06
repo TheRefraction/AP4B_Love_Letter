ac7b2376653cd7736f40ba9c5377d1e3bad7538d
Geemiinii <theorigal39@gmail.com>
2026-01-06 17:43:14 +0100

    implemented guard with swing

--- src/fr/utbm/loveletter/objects/card/Guard.java
+++ src/fr/utbm/loveletter/objects/card/Guard.java
@@ -5,6 +5,8 @@ import fr.utbm.loveletter.sprites.Sprite;
 
 import java.util.ArrayList;
 
+import javax.swing.JOptionPane;
+
 public class Guard extends Card {
 
     public Guard(Sprite sprite) {
@@ -15,8 +17,51 @@ public class Guard extends Card {
     @Override
     public void playEffect(ArrayList<Player> players, int ownerId) {
         System.out.println("Le joueur " + players.get(ownerId).toString() + " joue un Garde !");
-        //a faire : Ouvrir l'interface pour choisir un joueur cible
+        Player owner  = players.get(ownerId);
+        ArrayList<Player> canSeeHand = new ArrayList<>();
+        for (Player p : players) {
+            if (p != owner && !p.getEliminated() && !p.getProtected()) {
+                canSeeHand.add(p);
+            }
+        }
+        Player[] playersArray = canSeeHand.toArray(new Player[0]);
+        Player target = (Player) JOptionPane.showInputDialog(
+                null,
+                "Choisissez un joueur à cibler :",
+                "Effet du Garde",
+                JOptionPane.QUESTION_MESSAGE,
+                null,
+                playersArray,
+                playersArray[0]
+        );
 
+        String[] cardValues = {"0 - Espionne", "2 - Prêtre", "3 - Baron", "4 - Servante", "5 - Prince", "6 - Chancelier", "7 - Roi", "8 - Comtesse", "9 - Princesse"};
+
+        String selectedCardString = (String) JOptionPane.showInputDialog(
+                null,
+                "Quelle carte pensez-vous que " + target.toString() + " possède ?",
+                "Effet du Garde",
+                JOptionPane.QUESTION_MESSAGE,
+                null,
+                cardValues,
+                cardValues[0]
+        );
+
+        if (selectedCardString == null) return;
+
+        //recupere le 1er chiffre du txt (a verifier)
+        int guessedValue = Integer.parseInt(selectedCardString.substring(0, 1));
+
+        if (!target.getHand().isEmpty()) {
+            Card targetCard = target.getHand().get(0);
+
+            if (targetCard.getValue() == guessedValue) {
+                JOptionPane.showMessageDialog(null, "Bien joué ! " + target.toString() + " avait bien un " + targetCard.getName() + ".\nIl est éliminé");
+                target.setEliminated(true); // the player is eliminated
+            } else {
+                JOptionPane.showMessageDialog(null, "Non, " + target.toString() + " n'a pas cette carte.");
+            }
+        }
     }
 }
 
